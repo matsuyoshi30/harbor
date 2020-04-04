7dda3520adc11b5f7d65fa4ee4125a2b52d77326
matsuyoshi <sfbgwm30@gmail.com>
2020-04-05 01:16:01 +0900

    fix misc

--- static/src/theme.js
+++ static/src/theme.js
@@ -2,7 +2,6 @@ window.onload = function() {
     var toggle = document.getElementById("dark-mode-toggle");
 
     var darkTheme = document.getElementById("dark-mode-theme");
-    var darkThemeSys = document.getElementById("dark-mode");
 
     // the default theme is light
     var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
@@ -21,11 +20,9 @@ window.onload = function() {
 
         if (mode === "dark") {
             darkTheme.disabled = false;
-            darkThemeSys.disabled = false;
             toggle.className = "fas fa-sun";
         } else if (mode === "light") {
             darkTheme.disabled = true;
-            darkThemeSys.disabled = true;
             toggle.className = "fas fa-moon";
         }
     }
