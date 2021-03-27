d26017c6bef8141a3ab16e16226dcc442c0dd515
Masaya Watanabe <sfbgwm30@gmail.com>
2021-03-27 22:51:01 +0900

    Revert "Fix fonts do not download"

--- webpack.common.js
+++ webpack.common.js
@@ -1,27 +1,23 @@
 const path = require('path')
 
 module.exports = {
-  entry: {"main.js" : ['./static/src/main.js', './static/src/theme.js']},
-  output: {
-    path: `${__dirname}/static/js/`,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css/,
-        use: [
-          "style-loader",
+    entry: {"main.js" : ['./static/src/main.js', './static/src/theme.js']},
+    output: {
+        path: `${__dirname}/static/js/`,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
           {
-            loader: "css-loader",
-            options: { url: false }
+            test: /\.css/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: { url: false }
+              }
+            ]
           }
         ]
-      },
-      {
-        test: /\.(woff|woff2)$/,
-        type: "asset/inline",
-      },
-    ]
-  }
+      }
 };
